012f1f3c4dc00817b3a82ca9a5c9857009753876
Michael Washburn <mdw7326@rit.edu>
2016-04-23 17:50:15 -0400

    Super Microscopic, not working, Dropwizard API

---
+++ idea/src/main/java/com/app/idea/config/IdeaAppConfiguration.java
@@ -0,0 +1,11 @@
+package com.app.idea.config;
+
+import io.dropwizard.Configuration;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class IdeaAppConfiguration extends Configuration {
+	
+	
+
+}
